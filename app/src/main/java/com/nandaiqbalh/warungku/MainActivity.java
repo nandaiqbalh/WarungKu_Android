b75e0255fa7d225df17098d5b1782af08af79878
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-05-16 23:52:42 +0700

    Menambahkan aksi ketika bottom nav bar diklik

--- app/src/main/java/com/nandaiqbalh/warungku/MainActivity.java
+++ app/src/main/java/com/nandaiqbalh/warungku/MainActivity.java
@@ -1,30 +1,52 @@
 package com.nandaiqbalh.warungku;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.nandaiqbalh.warungku.fragment.AkunFragment;
 import com.nandaiqbalh.warungku.fragment.HomeFragment;
 import com.nandaiqbalh.warungku.fragment.KeranjangFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    Fragment homeFragment = new HomeFragment();
-    Fragment akunFragment = new AkunFragment();
-    Fragment keranjangFragment = new KeranjangFragment();
-    FragmentManager fm = getSupportFragmentManager();
-    Fragment active = homeFragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fm.beginTransaction().add(R.id.container, homeFragment).show(homeFragment).commit();
-        fm.beginTransaction().add(R.id.container, akunFragment).hide(akunFragment).commit();
-        fm.beginTransaction().add(R.id.container, keranjangFragment).hide(keranjangFragment).commit();
+        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.navigation_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.navigation_keranjang:
+                            selectedFragment = new KeranjangFragment();
+                            break;
+                        case R.id.navigation_akun:
+                            selectedFragment = new AkunFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
