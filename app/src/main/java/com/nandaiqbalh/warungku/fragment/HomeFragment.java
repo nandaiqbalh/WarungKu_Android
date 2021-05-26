aa6411153b502f725bde7c1a985a4f0fcd1bb8fb
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-05-26 17:50:05 +0700

    Menambahkan recycler view (produk terbaru, olahraga, elektronik) pada home fragment -> home fragment belum scrollable ke bawah

--- app/src/main/java/com/nandaiqbalh/warungku/fragment/HomeFragment.java
+++ app/src/main/java/com/nandaiqbalh/warungku/fragment/HomeFragment.java
@@ -89,25 +89,91 @@ public class HomeFragment extends Fragment {
         sliderLayout.setScrollTimeInSec(1);
         setSliderViews();
 
-        // recycle view
-        recyclerView = view.findViewById(R.id.rv_produk);
+        // TODO: RECYCLER VIEW PRODUK TERBARU
+        recyclerView = view.findViewById(R.id.rv_produk_terbaru);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
         dataHolder = new ArrayList<>();
-        Produk produk1 = new Produk("Adidas Nemeziz 18", "Rp 1.350.000", R.drawable.produk_sepatu_adidas_nemeziz_18);
-        dataHolder.add(produk1);
-        Produk produk2 = new Produk("Jersey Arsenal HOME 2020-2021", "Rp 350.000", R.drawable.produk_jersey_arsenal_home_2020_2021);
-        dataHolder.add(produk2);
-        Produk produk3 = new Produk("Adidas ONE", "Rp 1.400.000", R.drawable.produk_sepatu_adidas_one);
-        dataHolder.add(produk3);
-        Produk produk4 = new Produk("Nike Phantom GT Pro FG", "Rp 1.200.000", R.drawable.produk_sepatu_nike_phantom_gt_pro_fg);
-        dataHolder.add(produk4);
-        Produk produk5 = new Produk("Macbook PRO 2020", "Rp 18.999.000", R.drawable.produk_macbook_pro_2020);
-        dataHolder.add(produk5);
+        Produk terbaru1 = new Produk("Adidas Nemeziz 18 || Football", "Rp 1.350.000", R.drawable.produk_sepatu_adidas_nemeziz_18);
+        dataHolder.add(terbaru1);
+        Produk terbaru2 = new Produk("Jersey Arsenal HOME 2020-2021", "Rp 350.000", R.drawable.produk_jersey_arsenal_home_2020_2021);
+        dataHolder.add(terbaru2);
+        Produk terbaru3 = new Produk("Adidas ONE || Football", "Rp 1.400.000", R.drawable.produk_sepatu_adidas_one);
+        dataHolder.add(terbaru3);
+        Produk terbaru4 = new Produk("Nike Phantom GT Pro FG || Football", "Rp 1.200.000", R.drawable.produk_sepatu_nike_phantom_gt_pro_fg);
+        dataHolder.add(terbaru4);
+        Produk terbaru5 = new Produk("Macbook PRO 2020", "Rp 18.999.000", R.drawable.produk_macbook_pro_2020);
+        dataHolder.add(terbaru5);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        layoutManager.setOrientation(RecyclerView.HORIZONTAL);
-        recyclerView.setLayoutManager(layoutManager);
+        LinearLayoutManager layoutManagerTerbaru = new LinearLayoutManager(getActivity());
+        layoutManagerTerbaru.setOrientation(RecyclerView.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManagerTerbaru);
+
+        recyclerView.setAdapter(new ProdukAdapter(dataHolder));
+
+        // TODO: RECYCLER VIEW PRODUK TERLARIS
+        recyclerView = view.findViewById(R.id.rv_produk_terlaris);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        dataHolder = new ArrayList<>();
+        Produk terlaris1 = new Produk("Specs ACC LightSpeed Reborn || Futsal", "Rp 599.000", R.drawable.produk_sepatu_specs_acc_lightspeedreborn);
+        dataHolder.add(terlaris1);
+        Produk terlaris2 = new Produk("Jersey Manchester United 3rd 2020-2021", "Rp 350.000", R.drawable.produk_jersey_mu_3rd_2020_2021);
+        dataHolder.add(terlaris2);
+        Produk terlaris3 = new Produk("ECO Tupperware 500 ml", "Rp 52.000", R.drawable.produk_botol_eco_tupperware);
+        dataHolder.add(terlaris3);
+        Produk terlaris4 = new Produk("Nike Phantom GT Pro FG", "Rp 1.200.000", R.drawable.produk_sepatu_nike_phantom_gt_pro_fg);
+        dataHolder.add(terlaris4);
+        Produk terlaris5 = new Produk("HP Asus Zenfone MaxPro M1 3Gb", "Rp 1.500.000", R.drawable.produk_hp_asus_zenfone_m1);
+        dataHolder.add(terlaris5);
+
+        LinearLayoutManager layoutManagerTerlaris = new LinearLayoutManager(getActivity());
+        layoutManagerTerlaris.setOrientation(RecyclerView.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManagerTerlaris);
+
+        recyclerView.setAdapter(new ProdukAdapter(dataHolder));
+
+        // TODO: RECYCLER VIEW PRODUK OLAHRAGA
+        recyclerView = view.findViewById(R.id.rv_produk_olahraga);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        dataHolder = new ArrayList<>();
+        Produk olahraga1 = new Produk("Adidas Nemeziz 18 || Football", "Rp 1.350.000", R.drawable.produk_sepatu_adidas_nemeziz_18);
+        dataHolder.add(olahraga1);
+        Produk olahraga2 = new Produk("Jersey Arsenal HOME 2020-2021", "Rp 350.000", R.drawable.produk_jersey_arsenal_home_2020_2021);
+        dataHolder.add(olahraga2);
+        Produk olahraga3 = new Produk("OrtusEight Jogosala Rampage || Futsal", "Rp 449.000", R.drawable.produk_sepatu_ortuseight_jogosalarampage);
+        dataHolder.add(olahraga3);
+        Produk olahraga4 = new Produk("Nike Phantom GT Pro FG || Football", "Rp 1.200.000", R.drawable.produk_sepatu_nike_phantom_gt_pro_fg);
+        dataHolder.add(olahraga4);
+        Produk olahraga5 = new Produk("Jersey Manchester United HOME 2020-2021", "Rp 350.000", R.drawable.produk_jersey_mu_home_2020_2021);
+        dataHolder.add(olahraga5);
+
+        LinearLayoutManager layoutManagerOlahraga = new LinearLayoutManager(getActivity());
+        layoutManagerOlahraga.setOrientation(RecyclerView.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManagerOlahraga);
+
+        recyclerView.setAdapter(new ProdukAdapter(dataHolder));
+
+        // TODO: RECYCLER VIEW PRODUK ELEKTRONIK
+        recyclerView = view.findViewById(R.id.rv_produk_elektronik);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        dataHolder = new ArrayList<>();
+        Produk elektronik1 = new Produk("DELL Vostro 14 | 3401", "Rp 7.350.000", R.drawable.produk_dell_vostro_14_3401);
+        dataHolder.add(elektronik1);
+        Produk elektronik2 = new Produk("Asus ROG STRIX III-G", "Rp 15.999.000", R.drawable.produk_asus_rog);
+        dataHolder.add(elektronik2);
+        Produk elektronik3 = new Produk("iPod Touch 32GB", "Rp 1.400.000", R.drawable.produk_ipod_touch_32_gb);
+        dataHolder.add(elektronik3);
+        Produk elektronik4 = new Produk("HP Asus Zenfone MaxPro M1 3Gb", "Rp 1.500.000", R.drawable.produk_hp_asus_zenfone_m1);
+        dataHolder.add(elektronik4);
+        Produk elektronik5 = new Produk("Macbook PRO 2020", "Rp 18.999.000", R.drawable.produk_macbook_pro_2020);
+        dataHolder.add(elektronik5);
+
+        LinearLayoutManager layoutManagerElektronik = new LinearLayoutManager(getActivity());
+        layoutManagerElektronik.setOrientation(RecyclerView.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManagerElektronik);
 
         recyclerView.setAdapter(new ProdukAdapter(dataHolder));
 
