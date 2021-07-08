ed572401ae89df6f0550204732ba77b1194c22c7
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2021-07-08 23:45:25 +0700

    Merapikan validasi inputan user untuk registrasi

--- app/src/main/java/com/nandaiqbalh/warungku/activity/RegisterActivity.java
+++ app/src/main/java/com/nandaiqbalh/warungku/activity/RegisterActivity.java
@@ -2,7 +2,6 @@ package com.nandaiqbalh.warungku.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -16,10 +15,8 @@ import com.nandaiqbalh.warungku.rest.ApiClient;
 import com.nandaiqbalh.warungku.rest.RegisterRequest;
 import com.nandaiqbalh.warungku.rest.RegisterResponse;
 
-import java.io.IOException;
 import java.util.regex.Pattern;
 
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -28,6 +25,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     private Button btnRegister;
     private TextInputEditText edtName, edtEmail, edtPhone, edtPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,7 +47,6 @@ public class RegisterActivity extends AppCompatActivity {
                 registerRequest.setEmail(edtEmail.getText().toString());
                 registerRequest.setPhoneNumber(edtPhone.getText().toString());
                 registerRequest.setPassword(edtPassword.getText().toString());
-
                 register(registerRequest);
 
             }
@@ -57,9 +54,11 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
-    public void register(RegisterRequest registerRequest) {
+    private void register(RegisterRequest registerRequest) {
+
 
         String emailInput = edtEmail.getText().toString().trim(); // untuk validasi email
+        int phoneInput = edtPhone.getText().length(); // untuk validasi nomor telepon
         String passwordInput = edtPassword.getText().toString().trim(); // untuk validasi password
         final Pattern PASSWORD_PATTERN =
                 Pattern.compile("^" +
@@ -72,7 +71,6 @@ public class RegisterActivity extends AppCompatActivity {
                         ".{8,}" +               //at least 8 characters
                         "$");
 
-
         if (edtName.getText().toString().isEmpty()) {
             edtName.setError("Nama tidak boleh kosong!");
             edtName.requestFocus();
@@ -81,7 +79,7 @@ public class RegisterActivity extends AppCompatActivity {
             edtEmail.setError("Email tidak boleh kosong!");
             edtEmail.requestFocus();
             return;
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()){
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
             edtEmail.setError("Email yang anda masukkan tidak valid!");
             edtEmail.requestFocus();
             return;
@@ -89,22 +87,27 @@ public class RegisterActivity extends AppCompatActivity {
             edtPhone.setError("No Telepon tidak boleh kosong!");
             edtPhone.requestFocus();
             return;
-        } else if (edtPassword.getText().toString().isEmpty()) {
+        } else if (phoneInput < 10 || phoneInput > 13) {
+            edtPhone.setError("No Telepon tidak valid!");
+            edtPhone.requestFocus();
+            return;
+        } else if (passwordInput.isEmpty()) {
             edtPassword.setError("Password tidak boleh kosong!");
             edtPassword.requestFocus();
             return;
-        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()){
+        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
             edtPassword.setError("Password anda terlalu lemah! (Minimal 8 digit, angka + huruf).");
             edtPassword.requestFocus();
             return;
         }
+
 
         Call<RegisterResponse> registerResponseCall = ApiClient.getService().registerUser(registerRequest);
         registerResponseCall.enqueue(new Callback<RegisterResponse>() {
             @Override
             public void onResponse(Call<RegisterResponse> call, Response<RegisterResponse> response) {
 
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
 
                     String message = "Register successfully!";
                     Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_LONG).show();
